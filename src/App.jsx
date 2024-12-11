7e99b422097948db28a6b033e9cc100b9f2291a9
LuigiRac <lui.rac06@gmail.com>
2024-12-11 22:35:14 +0100

    add_tasks_length

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import HeaderComponent from "./Component/HeaderComponent.jsx";
 import tasks from "./data/tasks.js";
 
 
-const person = {
-
-}
 function App() {
   // parte logica
 
   function currentTasks() {
-    let currentItems = tasks.filter((task) => task.state.includes('backlog') || task.state.includes('in_progress')).map((task) => <li key={task.id} > <strong>{task.title}</strong> {task.state} Priority:{task.priority} Est. Time {task.estimatedTime}</ li>
-    )
+    let currentItems = tasks.filter((task) => task.state.includes('backlog') || task.state.includes('in_progress')).map((task) =>
+
+      <li key={task.id} >
+        <strong>{task.title}</strong> {task.state} Priority:{task.priority} Est. Time {task.estimatedTime}
+      </ li>);
 
     return currentItems
+
   };
+
 
   function completedTasks() {
     let completedItems = tasks.filter((task) => task.state.includes('completed')).map((task) => <li key={task.id} > <strong>{task.title}</strong> {task.state} Priority:{task.priority} Est. Time {task.estimatedTime}</ li>
@@ -32,10 +34,10 @@ function App() {
       <HeaderComponent />
 
       <main className="container">
-        <h2>Current Tasks</h2>
+        <h2>Current Tasks ({currentTasks().length})</h2>
         <ul>{currentTasks()}</ul>
 
-        <h2>Completed Tasks</h2>
+        <h2>Completed Tasks ({completedTasks().length})</h2>
         <ul>{completedTasks()}</ul>
       </main>
 
