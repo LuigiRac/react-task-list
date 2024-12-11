d02ec36cafcaae87104bb8251a136d2e7a978217
LuigiRac <lui.rac06@gmail.com>
2024-12-11 19:58:08 +0100

    impostazioni iniziali + bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // parte logica
+  const title = "Hello Word!";
+  const block = "Main";
+  const endPage = "footer";
 
+  //parte html
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1 className='text-danger'>{title}</h1>
+      </header>
+
+      <main>{block}</main>
+
+      <footer>{endPage}</footer>
     </>
-  )
+
+  );
+
 }
 
-export default App
+export default App;
