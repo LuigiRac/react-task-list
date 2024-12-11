bfe803081b69c04b810302f619b7843b10dbb53f
LuigiRac <lui.rac06@gmail.com>
2024-12-11 22:53:46 +0100

    fixed_list_layout

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,10 @@ function App() {
     let currentItems = tasks.filter((task) => task.state.includes('backlog') || task.state.includes('in_progress')).map((task) =>
 
       <li key={task.id} >
-        <strong>{task.title}</strong> {task.state} Priority:{task.priority} Est. Time {task.estimatedTime}
+
+        <div> <strong>{task.title}</strong> {task.state} </div>
+        <div>Priority:{task.priority} </div>
+        <div>Est. Time {task.estimatedTime}</div>
       </ li>);
 
     return currentItems
@@ -20,8 +23,14 @@ function App() {
 
 
   function completedTasks() {
-    let completedItems = tasks.filter((task) => task.state.includes('completed')).map((task) => <li key={task.id} > <strong>{task.title}</strong> {task.state} Priority:{task.priority} Est. Time {task.estimatedTime}</ li>
-    )
+    let completedItems = tasks.filter((task) => task.state.includes('completed')).map((task) =>
+
+      <li key={task.id} >
+        <strong>{task.title}</strong>
+        <div>{task.state} </div>
+        <div>Priority:{task.priority} </div>
+        <div>Est. Time {task.estimatedTime}</div>
+      </ li>);
 
     return completedItems
   };
@@ -35,10 +44,13 @@ function App() {
 
       <main className="container">
         <h2>Current Tasks ({currentTasks().length})</h2>
-        <ul>{currentTasks()}</ul>
+        <ul className='list-style'>{currentTasks()}</ul>
+
+
+
 
         <h2>Completed Tasks ({completedTasks().length})</h2>
-        <ul>{completedTasks()}</ul>
+        <ul className='list-style'>{completedTasks()}</ul>
       </main>
 
     </>
